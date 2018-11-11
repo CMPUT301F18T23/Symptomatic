e1b139cd4b7740354dbccf2e3fe6fd3d6b219c3e
ahzam1 <ahzam1@ualberta.ca>
2018-11-10 20:00:17 -0700

    some work

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/CViewProblems.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/CViewProblems.java
@@ -2,6 +2,11 @@ package ca.ualberta.symptomaticapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class CViewProblems extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class CViewProblems extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cview_problems);
+        TextView numproblems = (TextView) findViewById(R.id.tv_ProbNum); //need to change this text when view button pressed
+        final Spinner patient = (Spinner) findViewById(R.id.sp_Patients); //need to get selections
+        Button viewproblems = (Button) findViewById(R.id.btn_View);
+        final Button viewcontactinfo = (Button) findViewById(R.id.btn_viewcontactinfo);
+        ListView problemview = (ListView) findViewById(R.id.lv_problems);
+
+        viewproblems.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // take current selected patient and populate list view using their problems
+                viewcontactinfo.setEnabled(true); //allow view contact info to be able to be pressed
+                String selection = patient.getSelectedItem().toString(); //get current selection
+
+            }
+        });
     }
 }
