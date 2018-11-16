770f3c0331de5a2b69fa49170f09c8edc92f35e4
N550 <nb@ualberta.ca>
2018-11-16 10:49:38 -0700

    fixed issues from last merge 	modified:   SymptomaticApp/.idea/misc.xml 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddProblemActivity.java 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ListViewAdapter.java 	modified:   SymptomaticApp/app/src/main/res/layout/activity_add_problem.xml 	modified:   SymptomaticApp/app/src/main/res/layout/custom_listview.xml

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddProblemActivity.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddProblemActivity.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ca.ualberta.symptomaticapp;
 
 
@@ -17,6 +17,11 @@ import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
+
+import ca.ualberta.symptomaticapp.ListProblemsActivity;
+import ca.ualberta.symptomaticapp.Problem;
+import ca.ualberta.symptomaticapp.ProblemListController;
+import ca.ualberta.symptomaticapp.R;
 
 
 public class AddProblemActivity extends AppCompatActivity {
@@ -115,119 +120,3 @@ public class AddProblemActivity extends AppCompatActivity {
 
 
 }
-=======
-package ca.ualberta.symptomaticapp;
-
-
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import java.util.Calendar;
-import java.util.Date;
-
-
-public class AddProblemActivity extends AppCompatActivity {
-
-    private DatePickerDialog.OnDateSetListener DateSetListener;
-    private int year;
-    private int month;
-    private int day;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_problem);
-
-    }
-
-    public void getDate(View v){
-        Button dateButton = findViewById(R.id.AddDateButton);
-        dateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(AddProblemActivity.this, android.R.style.Theme_DeviceDefault_Dialog, DateSetListener, year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
-                dialog.show();
-
-
-
-            }
-        });
-
-        DateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year1, int month1, int dayOfMonth1) {
-                year = year1;
-                month = month1;
-                day = dayOfMonth1;
-
-
-
-
-            }
-        };
-
-
-    }
-
-
-    public void addProblem(View v){
-        EditText editTextTitle = findViewById(R.id.EnterTitleEditText);
-        String title = editTextTitle.getText().toString();
-        EditText editTextDescription = findViewById(R.id.EnterDescriptionEditText);
-        String description = editTextDescription.getText().toString();
-        Calendar cal = Calendar.getInstance();
-        cal.set(year, month, day);
-        Boolean goodProblem = true;
-        if (title.length() == 0) {
-            AlertDialog.Builder noTitleDialog = new AlertDialog.Builder(AddProblemActivity.this);
-            noTitleDialog.setMessage("Title cannot be empty");
-            noTitleDialog.show();
-            goodProblem = false;
-        }
-        if (year == 0) {
-            AlertDialog.Builder noDateDialog = new AlertDialog.Builder(AddProblemActivity.this);
-            noDateDialog.setMessage("Date cannot be empty");
-            noDateDialog.show();
-            goodProblem = false;
-        }
-
-        if (description.length() == 0) {
-            AlertDialog.Builder noDescriptionDialog = new AlertDialog.Builder(AddProblemActivity.this);
-            noDescriptionDialog.setMessage("Description cannot be empty");
-            noDescriptionDialog.show();
-            goodProblem = false;
-        }
-
-        if (goodProblem == true) {
-            ProblemListController pt = new ProblemListController();
-            Problem newProblem = new Problem(title, cal.getTime(), description);
-            pt.addProblem(newProblem);
-            Toast.makeText(this, title + cal.getTime().toString() + description, Toast.LENGTH_SHORT).show();
-        }
-
-
-
-
-    }
-
-
-
-
-
-}
->>>>>>> 9ec94f6b4bef24c432d598131d99f732cfd0f355
--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ListViewAdapter.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ListViewAdapter.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ca.ualberta.symptomaticapp;
 
 import android.content.Context;
@@ -13,6 +13,11 @@ import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import ca.ualberta.symptomaticapp.AddRecordActivity;
+import ca.ualberta.symptomaticapp.Problem;
+import ca.ualberta.symptomaticapp.R;
+import ca.ualberta.symptomaticapp.ViewFullProblemActivity;
 
 public class ListViewAdapter extends BaseAdapter implements ListAdapter {
     ArrayList<Problem> problemList;
@@ -78,75 +83,3 @@ public class ListViewAdapter extends BaseAdapter implements ListAdapter {
     }
 }
 
-=======
-package ca.ualberta.symptomaticapp;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
-public class ListViewAdapter extends BaseAdapter implements ListAdapter {
-    ArrayList<Problem> problemList;
-    private Context context;
-
-    public ListViewAdapter(ArrayList<Problem> list, Context context) {
-        this.problemList = list;
-        this.context = context;
-    }
-
-    @Override
-    public int getCount() {
-        return problemList.size();
-    }
-
-    @Override
-    public Object getItem(int pos) {
-        return problemList.get(pos);
-    }
-
-    @Override
-    public long getItemId(int pos) {
-        return 0;
-    }
-
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-        if (view == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.custom_listview, null);
-        }
-
-        //Handle TextView and display string from your list
-        TextView listItemText = view.findViewById(R.id.list_item_string);
-        listItemText.setText(problemList.get(position).toString());
-
-        //Handle buttons
-        Button viewFullProblemButton = view.findViewById(R.id.viewFullProblemButton);
-        Button addRecordButton = view.findViewById(R.id.addRecordButton);
-
-        viewFullProblemButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
-        addRecordButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-            }
-        });
-
-        return view;
-    }
-}
-
->>>>>>> 9ec94f6b4bef24c432d598131d99f732cfd0f355
