5361be9b1ab087a2eb26528396683480c3cfbfd9
HelenG123 <gezahegn@ualberta.ca>
2018-11-15 22:32:42 -0700

    Working on saving the image to a new directory on phone. Still not working.

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddRecordActivity.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddRecordActivity.java
@@ -2,21 +2,33 @@ package ca.ualberta.symptomaticapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 public class AddRecordActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int PICK_IMAGE_REQUEST = 100; // to access the gallery to choose an image
     static final int REQUEST_IMAGE_CAPTURE = 1; // to access the camera to take an image
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath; // the photo's file path
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,16 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
         Spinner spinner = (Spinner) findViewById(R.id.ProblemsSpinner);
         spinner.setAdapter(problemAdapter);
 
+        // ------------------------------ WORKING ON SAVING PHOTOS -------------------------------
+        // CURRENT STATUS: Not working!
+        // Set the file path
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File outputFile = new File(path,"Symptomatic");
+        // If the directory doesn't exist, create it
+        if (!outputFile.exists()) {
+            outputFile.mkdir();
+        }
+
         final Button savedPhoto = findViewById(R.id.savedPhoto);
         final Button takePhoto = findViewById(R.id.takePhoto);
 
@@ -38,8 +60,10 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
 
 
 
+
     }
 
+    // ---------------------------------- PHOTO FUNCTIONALITY ----------------------------------
     public void onClick(View v){
         int viewId = v.getId();
         // Gets images from gallery - Reference: https://en.proft.me/2017/07/1/how-get-image-gallery-or-camera-android/
@@ -58,11 +82,14 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
+            File newPhotoFile = null;
+            try {
+                newPhotoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                System.out.println("Error in creating the file for the image");
+            }
         }
-
-
-
-
 
     }
 
@@ -70,10 +97,54 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch(requestCode){
             case PICK_IMAGE_REQUEST:
+
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = data.getData();
 
                 }
+        }
+    }
+
+    // TODO: Edit
+    // Reference: https://developer.android.com/training/camera/photobasics#java
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    // TODO: Edit
+    // Reference: https://developer.android.com/training/camera/photobasics#java
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                System.out.println("Error in creating the file for the image");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
