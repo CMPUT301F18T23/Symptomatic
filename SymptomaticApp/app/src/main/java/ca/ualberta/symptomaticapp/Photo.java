334385bb324a22d4865a1d4012cf061deaa53054
HelenG123 <gezahegn@ualberta.ca>
2018-11-15 20:41:26 -0700

    Finished constructing the base of the Photo class with a completed constructor, getters, and other methods.

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/Photo.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/Photo.java
@@ -7,7 +7,29 @@ public class Photo {
     protected Date timestamp;
     protected Integer photoSize;
 
-    public Photo(){
+    public Photo(String photoPath, Date timestamp, Integer photoSize){
+        this.photoPath = photoPath;
+        this.timestamp = timestamp;
+        this.photoSize = photoSize;
+    }
 
+    public String getPhotoPath() {
+        // Return the photo's file path as a String
+        return this.photoPath;
+    }
+
+    public Date getTimestamp() {
+        // Return the timestamp the photo was added as a Date
+        return this.timestamp;
+    }
+
+    public Integer getPhotoSize() {
+        // Return the photo's size as an Integer
+        return this.photoSize;
+    }
+
+    public void compressImage() {
+        // If the photo's size exceeds 65536 bytes, compress the image.
+        return;
     }
 }
