032cff20e262170149f9fa0a32ec8bebc05ed29b
Amy Xiang <33043340+amy-xiang@users.noreply.github.com>
2018-11-20 13:45:17 -0700

    Javadoc for Photo class

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/Photo.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/Photo.java
@@ -1,4 +1,15 @@
-/*
+package ca.ualberta.symptomaticapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
  * Photo.java
  *
  * Version 1
@@ -15,17 +26,6 @@
  *
  */
 
-package ca.ualberta.symptomaticapp;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Log;
-
-import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Photo {
     protected String photoPath;
     protected String timestamp;
@@ -34,6 +34,10 @@ public class Photo {
     protected Bitmap photoBitmap;
 //    protected int quality;
 
+    /**
+     * Constructor for photo
+     * @param photoBitmap
+     */
     public Photo(Bitmap photoBitmap){
         // Initialize the Photo object
         this.photoBitmap = photoBitmap;
@@ -55,6 +59,9 @@ public class Photo {
         this.checkBitmapSize();
     }
 
+    /**
+     * Checks if the bitmap size of the photo is under required size
+     */
     private void checkBitmapSize() {
         // for testing: Log.d("Checked Bitmap Size", "hello" );
         if (photoByteArray.length > 65536){
@@ -65,21 +72,37 @@ public class Photo {
 
     }
 
+    /**
+     * gets the String photoPath
+     * @return the String photoPath
+     */
     public String getPhotoPath() {
         // Return the photo's file path as a String
         return this.photoPath;
     }
 
+    /**
+     * gets the string TimeStamp
+     * @return string TimeStamp
+     */
     public String getTimestamp() {
         // Return the timestamp the photo was added as a Date
         return this.timestamp;
     }
 
+    /**
+     * gets integer photo size
+     * @return integer photoSize
+     */
     public Integer getPhotoSize() {
         // Return the photo's size as an Integer
         return this.photoSize;
     }
 
+    /**
+     * If initial photo size is larger than specified, compress the photo
+     * and do so continually until the photo is under the specified size
+     */
     public void compressPhoto() {
         // If the photo's size exceeds 65536 bytes, compress the image.
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -101,16 +124,28 @@ public class Photo {
 
     }
 
+    /**
+     * gets the byte array of the photo
+     * @return byte array of the photo
+     */
     public byte[] getPhotoByteArray (){
         // Return the byte array representation of the photo
         return this.photoByteArray;
     }
 
+    /**
+     * gets the bitmap of the photo
+     * @return bitmap of the photo
+     */
     public Bitmap getPhotoBitmap(){
         // Return the bitmap representation of the photo
         return this.photoBitmap;
     }
 
+    /**
+     * sets the size of the photo
+     * @param photoSize
+     */
     public void setPhotoSize(int photoSize) {
         // Return the size of the photo
         this.photoSize = photoSize;
