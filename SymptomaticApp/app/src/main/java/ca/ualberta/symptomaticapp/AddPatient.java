8d591f2d87eba4f1eb9350892b4bcb57cf28b807
N550 <nb@ualberta.ca>
2018-11-19 19:11:07 -0700

    Changes to caregiver layouts 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddPatient.java 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/CaregiverHome.java 	modified:   SymptomaticApp/app/src/main/res/layout/activity_add_patient.xml 	modified:   SymptomaticApp/app/src/main/res/layout/activity_caregiver_home.xml 	new file:   SymptomaticApp/app/src/main/res/menu/add_patient_menu.xml
    	new file:   SymptomaticApp/app/src/main/res/menu/caregiver_home_menu.xml

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddPatient.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/AddPatient.java
@@ -8,6 +8,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +31,11 @@ public class AddPatient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_patient);
+        Toolbar toolbar = findViewById(R.id.addPatient_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Add Patient");
+
+
         final Caregiver caregiver = Login.thisCaregiver;
         Button cancel = (Button) findViewById(R.id.btn_Cancel);
         Button addpatient = (Button) findViewById(R.id.btn_addpatient);
@@ -80,5 +89,27 @@ public class AddPatient extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_patient_menu, menu);
+        return true;
+    }
+    public void viewHome(MenuItem menu) {
+        Intent intent = new Intent(AddPatient.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewAddPatients(MenuItem menu) {
+        Intent intent = new Intent(AddPatient.this, AddPatient.class);
+        startActivity(intent);
+    }
+    public void viewLogout(MenuItem menu){
+        Login.thisCaregiver = null;
+        Login.thisUser = null;
+        Intent intent = new Intent(AddPatient.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/CaregiverHome.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/CaregiverHome.java
@@ -3,6 +3,10 @@ package ca.ualberta.symptomaticapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,14 +17,17 @@ public class CaregiverHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caregiver_home);
-        TextView hellouser = (TextView) findViewById(R.id.tv_hellouser);
+        Toolbar toolbar = findViewById(R.id.caregiver_home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home Page");
+
+        TextView hellouser = (TextView) findViewById(R.id.HelloCareProviderTextView);
         hellouser.setText("Hello "+ Login.thisCaregiver.returnUsername() + "!");
 
-        Button addpat = (Button) findViewById(R.id.btn_addpatient);
-        Button viewpat = (Button) findViewById(R.id.btn_viewpatients);
-        Button openviewprob = (Button) findViewById(R.id.btn_openviewproblems);
-        Button openviewrecords = (Button) findViewById(R.id.btn_openviewrecords);
-        addpat.setOnClickListener(new View.OnClickListener() {
+        Button ViewPatientButton = (Button) findViewById(R.id.ViewPatientButton);
+        Button AddPatientButton = (Button) findViewById(R.id.AddPatientButton);
+
+        ViewPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CaregiverHome.this, AddPatient.class);
@@ -28,7 +35,7 @@ public class CaregiverHome extends AppCompatActivity {
             }
         });
 
-        viewpat.setOnClickListener(new View.OnClickListener() {
+        AddPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CaregiverHome.this, ViewPatients.class);
@@ -36,20 +43,29 @@ public class CaregiverHome extends AppCompatActivity {
             }
         });
 
-        openviewprob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CaregiverHome.this, CViewProblems.class);
-                startActivity(intent);
-            }
-        });
+    }
 
-        openviewrecords.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CaregiverHome.this, CViewRecords.class);
-                startActivity(intent);
-            }
-        });
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.caregiver_home_menu, menu);
+        return true;
+    }
+    public void viewViewPatients(MenuItem menu) {
+        Intent intent = new Intent(CaregiverHome.this, ViewPatients.class);
+        startActivity(intent);
+    }
+
+    public void viewAddPatients(MenuItem menu) {
+        Intent intent = new Intent(CaregiverHome.this, AddPatient.class);
+        startActivity(intent);
+    }
+
+    public void viewLogout(MenuItem menu){
+        Login.thisCaregiver = null;
+        Login.thisUser = null;
+        Intent intent = new Intent(CaregiverHome.this, MainActivity.class);
+        startActivity(intent);
     }
 }
