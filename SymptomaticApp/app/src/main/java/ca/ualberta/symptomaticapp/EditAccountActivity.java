99433a7db84db6efa024fe23447c7be0f08727b9
N550 <nb@ualberta.ca>
2018-11-21 20:26:39 -0700

    Got started on edit account. 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/EditAccountActivity.java

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/EditAccountActivity.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/EditAccountActivity.java
@@ -7,8 +7,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class EditAccountActivity extends AppCompatActivity {
+
+    String UName;
+    String email;
+    String phoneNumber;
+
+    Boolean goodName;
+    Boolean goodEmail;
+    Boolean goodPhone;
+
+    String nameError;
+    String emailError;
+    String phoneError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,15 @@ public class EditAccountActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.editAccount_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Edit Account");
+
+        TextView usernameTextView = findViewById(R.id.usernameEditText);
+        usernameTextView.setText(Login.thisUser.username);
+
+        TextView emailTextView = findViewById(R.id.emailAddressEditText);
+        emailTextView.setText(Login.thisUser.email);
+
+        TextView phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+        phoneNumberEditText.setText(Login.thisUser.phone);
     }
 
     @Override
@@ -42,6 +65,39 @@ public class EditAccountActivity extends AppCompatActivity {
         Login.thisUser = null;
         Intent intent = new Intent(EditAccountActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void editInfo(View V) {
+
+        TextView usernameTextView = findViewById(R.id.usernameEditText);
+        UName = usernameTextView.getText().toString();
+
+        TextView emailTextView = findViewById(R.id.emailAddressEditText);
+        email = emailTextView.getText().toString();
+
+        TextView phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+        phoneNumber = phoneNumberEditText.getText().toString();
+
+        // Check for bad username
+        if (UName.length() == 0){
+            goodName = false;
+            nameError = "Username cannot be empty.";
+        } else if (UName.length() < 8){
+            goodName = true;
+            nameError = "Username size must be a minimum of 8 characters.";
+        }
+
+        // Check for bad email
+        if (email.length() == 0){
+            goodEmail = false;
+            emailError = "Email cannot be empty";
+        }
+
+
+
+
+
+
     }
 
 }
