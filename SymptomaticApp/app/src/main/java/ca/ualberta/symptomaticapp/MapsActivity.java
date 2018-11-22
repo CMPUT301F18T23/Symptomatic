2f4d9fbb35f5fd97407cf8fcff48e2ac7f4a9376
N550 <nb@ualberta.ca>
2018-11-22 00:49:55 -0700

    modified:   SymptomaticApp/.idea/caches/build_file_checksums.ser 	modified:   SymptomaticApp/app/src/debug/res/values/google_maps_api.xml 	modified:   SymptomaticApp/app/src/main/AndroidManifest.xml 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/MapsActivity.java 	modified:   SymptomaticApp/app/src/main/res/values/strings.xml

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/MapsActivity.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng edmonton = new LatLng(53.631611, -113.323975);
+        mMap.addMarker(new MarkerOptions().position(edmonton).title("Marker in Edmonton"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(edmonton));
     }
 }
