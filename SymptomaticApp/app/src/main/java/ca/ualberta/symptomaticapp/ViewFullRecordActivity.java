dfcd1a8325bda074ebaccd7c1421f0c64dc44907
N550 <nb@ualberta.ca>
2018-11-28 19:36:46 -0700

    Got maps working in view full record 	modified:   SymptomaticApp/.idea/caches/build_file_checksums.ser 	modified:   SymptomaticApp/app/src/main/AndroidManifest.xml 	new file:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/MapOfSingleRecordActivity.java 	modified:   SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ViewFullRecordActivity.java 	new file:   SymptomaticApp/app/src/main/res/layout/activity_map_of_single_record.xml 	modified:   SymptomaticApp/app/src/main/res/values/strings.xml

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ViewFullRecordActivity.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ViewFullRecordActivity.java
@@ -1,5 +1,6 @@
 package ca.ualberta.symptomaticapp;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class ViewFullRecordActivity extends AppCompatActivity {
 
@@ -23,6 +28,9 @@ public class ViewFullRecordActivity extends AppCompatActivity {
 
         record = (Record) getIntent().getSerializableExtra("record");
 
+        //record.geolocation = new LatLng(-34, 151);
+
+
         // Insert the records problem
         TextView problemTextView = findViewById(R.id.InputProblemTextView);
         problemTextView.setText(record.problem);
@@ -34,6 +42,26 @@ public class ViewFullRecordActivity extends AppCompatActivity {
         // Insert the records time stamp
         TextView timeStampTextView = findViewById(R.id.InputDateTextView);
         timeStampTextView.setText(record.recordDate.toString());
+
+        Button mapButton = findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (record.geolocation != null) {
+                    Intent intent = new Intent(ViewFullRecordActivity.this, MapOfSingleRecordActivity.class);
+                    intent.putExtra("geolocation", record.geolocation);
+                    intent.putExtra("title", record.recordTitle);
+                    startActivity(intent);
+                }
+                else{
+                    // No geolocation exists, must add in edit record
+                    AlertDialog.Builder noGeolocationDialog = new AlertDialog.Builder(ViewFullRecordActivity.this);
+                    noGeolocationDialog.setMessage("No Geolocation exists. You may add a geolocation in Edit Record.");
+                    noGeolocationDialog.show();
+                }
+            }
+        });
+
 
 
 
