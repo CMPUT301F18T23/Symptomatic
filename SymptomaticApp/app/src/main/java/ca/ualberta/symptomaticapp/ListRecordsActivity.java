73abbd7177c46450e702c537a1b09e16d522f830
pineo <pineo@ualberta.ca>
2018-12-02 18:23:06 -0700

    fix??

--- SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ListRecordsActivity.java
+++ SymptomaticApp/app/src/main/java/ca/ualberta/symptomaticapp/ListRecordsActivity.java
@@ -164,13 +164,12 @@ public class ListRecordsActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
         final CollectionReference records = db.collection("records");
-
+        thisRecordList.empty();
         Query recordsQuery = records.whereEqualTo("problem",problem.getTitle()).whereEqualTo("user",Login.thisUser.returnUsername());
         recordsQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
-                    thisRecordList.empty();
                     int x = 0;
                     for(QueryDocumentSnapshot document: task.getResult()){
                         thisRecordList.addRecord(document.toObject(Record.class));
